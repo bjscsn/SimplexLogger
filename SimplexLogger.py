af70186bb12bb66abac49222d49797885ac39349
bjscsn <bjoern.schoof@schoofnet.de>
2023-05-18 16:18:47 +0200

    Added timestamp to logging

--- SimplexLogger.py
+++ SimplexLogger.py
@@ -4,12 +4,13 @@ SimplexLogger.py
 
 SimplexLogger is a unidirectional single-threaded logger. The idea was derived from the code found at https://stackoverflow.com/questions/19231465/how-to-make-a-serial-port-sniffer-sniffing-physical-port-using-a-python.
 """
-__build__ = "104"
+__build__ = "105"
 __created__ = "2023-05-18_12-59-19"
-__updated__ = "2023-05-18_15-51-34"
+__updated__ = "2023-05-18_16-18-03"
 
 # GLOBAL
 CRLF = "\n"
+CR = "\r"
 NOCRLF = ""
 
 class SimplexLogger():
@@ -28,6 +29,15 @@ class SimplexLogger():
         self.com_port = str
         self.log_filename = ""
 
+    def _getTimeStamp(self)->str:
+        import datetime
+        timestamp = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d_%H:%M:%M.%f")
+        return timestamp
+    
+    def _toLogRecord(self, data, separator=": ")->str:
+        result = "{}{}{}".format(self._getTimeStamp(), separator, data)
+        return result
+
     def run(self)->None:
         print("Monitor running. Stop with CTRL-C.")
         serial=None
@@ -37,20 +47,27 @@ class SimplexLogger():
         
         if self.log_filename:
             with open(self.log_filename, "+a") as log_file:
+                print(self._toLogRecord(data=">>> Session Start <<<"))
+                log_file.write(self._toLogRecord(data=">>> Session Start <<<", separator=",") + CRLF)
                 try:
                     while True:
                         buffer = listener.read_until()
-                        print(buffer.decode('utf-8').strip())
-                        log_file.write(buffer.decode('utf-8').strip() + CRLF)
+                        print(self._toLogRecord(buffer.decode('utf-8').strip()), end=CR)
+                        log_file.write(self._toLogRecord(data=buffer.decode('utf-8').strip(), separator=",") + CRLF)
                 except KeyboardInterrupt as ki:
+                    print()
+                    print(self._toLogRecord(data=">>> Session End <<<"))
+                    log_file.write(self._toLogRecord(data=">>> Session End <<<", separator=",") + CRLF)
                     print("Closing log file '{}'.".format(self.log_filename))
                     log_file.close()
         else:
+            print(">>> Session Start {} <<<".format(self._getTimeStamp()))
             try:
                 while True:
                     buffer = listener.read_until()
                     print(buffer.decode('utf-8').strip())
             except KeyboardInterrupt as ki:
+                print(">>> Session End {} <<<".format(self._getTimeStamp()))
                 print("Closing..")
            
     def __str__(self)->str:
